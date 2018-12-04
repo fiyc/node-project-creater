fdc8153874f8781ff636e67fc24af854f70aaae1
fiyc <yifei.chen@harmontroncs.com>
2018-12-04 11:05:28 +0800

    init project

---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+/**
+* @Author: fiyc
+* @Date : 2018-12-04 10:15
+* @FileName : index.js
+* @Description : 
+    - 入口文件
+    - 设置命令行指令
+*/
+
+const packageInfo = require('./package.json');
+let programe = require('commander');
+let control = require('./src/command-control');
+
+/**
+ * 工具版本
+ */
+programe.version(packageInfo.version);
+
+/**
+ * 初始化项目目录
+ * 如果无参数, 则为当前目录
+ */
+programe.command('init [dir]')
+    .description('初始化项目目录')
+    .action(dir => {
+        control.init(dir);
+    });
+
+/**
+ * 生产项目 
+ */
+programe.command('build [version]')
+    .description('生成项目')
+    .action(version => {
+        control.build(version);
+    });
+
+programe.parse(process.argv);
+
+
+
+
+
---
+++ src/command-control.js
@@ -0,0 +1,20 @@
+/**
+* @Author: fiyc
+* @Date : 2018-12-04 11:00
+* @FileName : command-control.js
+* @Description : 
+    - 指令控制模块
+*/
+
+let init  = function(targetPath){
+
+}
+
+let build = function(version){
+
+}
+
+module.exports = {
+    init,
+    build
+};
